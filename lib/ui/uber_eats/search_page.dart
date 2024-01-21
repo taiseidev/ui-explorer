87cd2a257cdf3348f9cde0cb4b31a22932a04a15
onishi taisei <taisei.developer@gmail.com>
2024-01-21 23:29:51 +0900

    🚀 feat: Uberの検索画面を作成

--- lib/ui/uber_eats/search_page.dart
+++ lib/ui/uber_eats/search_page.dart
@@ -10,190 +10,200 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            centerTitle: false,
-            pinned: true,
-            floating: true,
-            expandedHeight: 110,
-            backgroundColor: Colors.white,
-            surfaceTintColor: Colors.white,
-            title: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'すぐに配達',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Color(0xff888988),
+      body: Scrollbar(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              centerTitle: false,
+              pinned: true,
+              floating: true,
+              expandedHeight: 110,
+              backgroundColor: Colors.white,
+              surfaceTintColor: Colors.white,
+              title: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'すぐに配達',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xff888988),
+                    ),
                   ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      '東京スカイツリー',
-                      style: TextStyle(
-                        fontSize: 14,
+                  Row(
+                    children: [
+                      Text(
+                        '東京スカイツリー',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff3d3d3d),
+                        ),
+                      ),
+                      Icon(Icons.keyboard_arrow_down_outlined)
+                    ],
+                  ),
+                ],
+              ),
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(60),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 4, 16, 8),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Uber Eats を検索',
+                      hintStyle: const TextStyle(
                         fontWeight: FontWeight.bold,
-                        color: Color(0xff3d3d3d),
+                        color: Color(0xff4d4d4d),
+                      ),
+                      contentPadding: EdgeInsets.zero,
+                      prefixIcon: const Icon(Icons.search),
+                      filled: true,
+                      fillColor: const Color(0xfff3f2f2),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(30),
                       ),
                     ),
-                    Icon(Icons.keyboard_arrow_down_outlined)
+                  ),
+                ),
+              ),
+            ),
+            const SliverPadding(
+              padding: EdgeInsets.only(top: 16),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _Category(
+                      title: 'レストラン',
+                      icon: Image.asset(
+                        Assets.ui.uberEats.png.cameraDynamicColor.path,
+                        width: 50,
+                      ),
+                      onTap: () async {},
+                    ),
+                    _Category(
+                      title: '食品・生鮮食品',
+                      icon: Image.asset(
+                        Assets.ui.uberEats.png.headphoneDynamicColor.path,
+                        width: 50,
+                      ),
+                      onTap: () {},
+                    ),
+                    _Category(
+                      title: 'お酒',
+                      icon: Image.asset(
+                        Assets.ui.uberEats.png.micDynamicColor.path,
+                        width: 50,
+                      ),
+                      onTap: () {},
+                    ),
+                    _Category(
+                      title: 'すべて表示',
+                      icon: const Icon(
+                        Icons.more_horiz,
+                        size: 36,
+                      ),
+                      onTap: () {},
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
-            bottom: PreferredSize(
-              preferredSize: const Size.fromHeight(60),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(16, 4, 16, 8),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Uber Eats を検索',
-                    hintStyle: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff4d4d4d),
+            const SliverPadding(
+              padding: EdgeInsets.only(top: 16),
+            ),
+            const SliverToBoxAdapter(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(width: 16),
+                    _Brand(
+                      imagePath:
+                          'https://www.ministop.co.jp/corporate/about/figure/brand_im01.gif',
                     ),
-                    contentPadding: EdgeInsets.zero,
-                    prefixIcon: const Icon(Icons.search),
-                    filled: true,
-                    fillColor: const Color(0xfff3f2f2),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
+                    SizedBox(width: 16),
+                    _Brand(
+                      imagePath:
+                          'http://www.marksign.net/wp-content/uploads/2016/04/lawson.jpg',
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(30),
+                    SizedBox(width: 16),
+                    _Brand(
+                      imagePath:
+                          'https://logo.jp/concour/wp-content/uploads/2012/04/godiva.gif',
                     ),
-                  ),
+                    SizedBox(width: 16),
+                    _Brand(
+                      imagePath:
+                          'http://bachohima524.xsrv.jp/wp-content/uploads/2021/06/%E3%82%B5%E3%83%B3%E3%83%89%E3%83%A9%E3%83%83%E3%82%B0%E3%80%80%E3%82%B5%E3%83%A0%E3%83%8D.png',
+                    ),
+                    SizedBox(width: 16),
+                    _Brand(
+                      imagePath:
+                          'http://www.er-planning.co.jp/wp/wp-content/uploads/2020/04/costco-logo-1200x491.jpg',
+                    ),
+                    SizedBox(width: 16),
+                    CircleAvatar(
+                      backgroundColor: Color(0xfff3f2f2),
+                      maxRadius: 25,
+                      child: Icon(
+                        Icons.arrow_forward,
+                        size: 16,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
                 ),
               ),
             ),
-          ),
-          const SliverPadding(
-            padding: EdgeInsets.only(top: 16),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _Category(
-                    title: 'レストラン',
-                    icon: Image.asset(
-                      Assets.ui.uberEats.png.cameraDynamicColor.path,
-                      width: 50,
-                    ),
-                    onTap: () {},
-                  ),
-                  _Category(
-                    title: '食品・生鮮食品',
-                    icon: Image.asset(
-                      Assets.ui.uberEats.png.headphoneDynamicColor.path,
-                      width: 50,
-                    ),
-                    onTap: () {},
-                  ),
-                  _Category(
-                    title: 'お酒',
-                    icon: Image.asset(
-                      Assets.ui.uberEats.png.micDynamicColor.path,
-                      width: 50,
-                    ),
-                    onTap: () {},
-                  ),
-                  _Category(
-                    title: 'すべて表示',
-                    icon: const Icon(
-                      Icons.more_horiz,
-                      size: 36,
-                    ),
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SliverPadding(
-            padding: EdgeInsets.only(top: 16),
-          ),
-          const SliverToBoxAdapter(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  SizedBox(width: 16),
-                  _Brand(
-                    imagePath:
-                        'https://www.ministop.co.jp/corporate/about/figure/brand_im01.gif',
-                  ),
-                  SizedBox(width: 16),
-                  _Brand(
-                    imagePath:
-                        'http://www.marksign.net/wp-content/uploads/2016/04/lawson.jpg',
-                  ),
-                  SizedBox(width: 16),
-                  _Brand(
-                    imagePath:
-                        'https://logo.jp/concour/wp-content/uploads/2012/04/godiva.gif',
-                  ),
-                  SizedBox(width: 16),
-                  _Brand(
-                    imagePath:
-                        'http://bachohima524.xsrv.jp/wp-content/uploads/2021/06/%E3%82%B5%E3%83%B3%E3%83%89%E3%83%A9%E3%83%83%E3%82%B0%E3%80%80%E3%82%B5%E3%83%A0%E3%83%8D.png',
-                  ),
-                  SizedBox(width: 16),
-                  _Brand(
-                    imagePath:
-                        'http://www.er-planning.co.jp/wp/wp-content/uploads/2020/04/costco-logo-1200x491.jpg',
-                  ),
-                  SizedBox(width: 16),
-                  CircleAvatar(
-                    backgroundColor: Color(0xfff3f2f2),
-                    maxRadius: 25,
-                    child: Icon(
-                      Icons.arrow_forward,
-                      size: 16,
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                ],
-              ),
-            ),
-          ),
-          const SliverPadding(
-            padding: EdgeInsets.symmetric(vertical: 4),
-          ),
-          const SliverToBoxAdapter(
-            child: Padding(
+            const SliverPadding(
               padding: EdgeInsets.symmetric(vertical: 4),
-              child: Divider(
-                color: Color(0xfff3f2f2),
+            ),
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 4),
+                child: Divider(
+                  color: Color(0xfff3f2f2),
+                ),
               ),
             ),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: _ListItem(),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: _ListItem(),
+                ),
+                childCount: 50,
               ),
-              childCount: 50,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        unselectedItemColor: const Color(0xff5d5e5d),
+        selectedItemColor: Colors.black,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedFontSize: 12,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'ホーム'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_to_drive_sharp), label: '雑貨'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '検索'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_rounded), label: 'カート'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_box_rounded), label: 'アカウント'),
         ],
       ),
     );
@@ -201,7 +211,7 @@ class SearchPage extends StatelessWidget {
 }
 
 class _ListItem extends HookWidget {
-  const _ListItem({super.key});
+  const _ListItem();
 
   @override
   Widget build(BuildContext context) {
